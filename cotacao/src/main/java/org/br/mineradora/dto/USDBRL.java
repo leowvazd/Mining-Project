7fec76bd2771a164dcddb6c83472f01044144dc2
Leonardo Vaz <leonardovazlourenco@gmail.com>
2023-05-23 21:31:24 -0300

    class 4 done

---
+++ cotacao/src/main/java/org/br/mineradora/dto/USDBRL.java
@@ -0,0 +1,27 @@
+package org.br.mineradora.dto;
+
+import io.quarkus.arc.All;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
+
+@Jacksonized
+@Data
+@Builder
+@AllArgsConstructor
+public class USDBRL {
+
+    public String code;
+    public String codein;
+    public String name;
+    public String high;
+    public String low;
+    public String varBid;
+    public String pctChange;
+    public String bid;
+    public String ask;
+    public String timestamp;
+    public String create_date;
+
+}
